3d23aedf939326af510270f95d8acfed3d20331f
Doron Oded <doron.oded@capriza.com>
2018-01-14 17:06:09 +0200

    bltester

--- test/BLTester/business-logic-tester.js
+++ test/BLTester/business-logic-tester.js
@@ -2,20 +2,17 @@ var fse = require ("fs-extra");
 var argv = require('minimist')(process.argv.slice(2));
 var log = msg => console.log(`[Business-Logic-Tester] ${msg}`);
 
-if (argv.bl) {
+//var blPath = `../../../business-logic/${argv.bl}`;
+//fse.copySync(`${blPath}/resources.json`, "../../resources.json");
+var bl = require (`${process.pwd}`);
 
-    //var blPath = `../../../business-logic/${argv.bl}`;
-    //fse.copySync(`${blPath}/resources.json`, "../../resources.json");
-    var bl = require (`${process.pwd}`);
+//1. make sure the bl exposes methods: "fetch, getApproval, approve, reject"
+var unimplementedActions = ["fetch", "getApproval", "approve", "reject"].filter (action => !Object.keys(bl).includes(action));
+if (unimplementedActions.length > 0)
+    log (`Error - '${bl}' doesn't implemented: ${unimplementedActions}`);
 
-    //1. make sure the bl exposes methods: "fetch, getApproval, approve, reject"
-    var unimplementedActions = ["fetch", "getApproval", "approve", "reject"].filter (action => !Object.keys(bl).includes(action));
-    if (unimplementedActions.length > 0)
-        log (`Error - '${bl}' doesn't implemented: ${unimplementedActions}`);
-
-    //2. start server
-    var server = require("./server.js");
-    server.registerActions(bl);
-    server.initBL();
-}
+//2. start server
+var server = require("./server.js");
+server.registerActions(bl);
+server.initBL();
 
