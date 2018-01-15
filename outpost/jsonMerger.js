cf3b36bd4644b09b6b69ae8c2d3812012ed7ffd2
Roie Uziel <roie.uziel@capriza.com>
2018-01-15 11:23:02 +0200

    Outpost

--- outpost/jsonMerger.js
+++ outpost/jsonMerger.js
@@ -3,4 +3,5 @@ var merge = require('deepmerge');
 var fileA = process.argv[2];
 var fileB = process.argv[3];
 var fileOut = process.argv[4];
-fs.writeFileSync(fileOut, JSON.stringify(merge(JSON.parse(fs.readFileSync(fileA)), JSON.parse(fs.readFileSync(fileB)))));
+var merged = merge(JSON.parse(fs.readFileSync(fileA)), JSON.parse(fs.readFileSync(fileB)));
+fs.writeFileSync(fileOut, JSON.stringify(merged, null, 2));
