bdeca485fa4b00a5120e5739d10ced147c02324d
Roie Uziel <roie.uziel@capriza.com>
2018-01-15 10:55:27 +0200

    Connectors refactor

--- outpost/jsonMerger.js
+++ outpost/jsonMerger.js
@@ -3,4 +3,4 @@ var merge = require('deepmerge');
 var fileA = process.argv[2];
 var fileB = process.argv[3];
 var fileOut = process.argv[4];
-fs.writeFileSync(fileOut, JSON.stringify(merge(require(fileA), require(fileB))));
+fs.writeFileSync(fileOut, JSON.stringify(merge(JSON.parse(fs.readFileSync(fileA)), JSON.parse(fs.readFileSync(fileB)))));
