aa59edbb12401b74d37ca508fb197704ca765964
Roie Uziel <roie.uziel@capriza.com>
2018-02-01 13:28:50 +0200

    Fix outpost module configuration

--- outpost/module/configure.js
+++ outpost/module/configure.js
@@ -1,11 +1,11 @@
-const fs = require("fs");
+/*const fs = require("fs");
 var url = require ("url");
-var server = url.parse(outpost.opconfig.fortitude);
+var server = url.parse(outpost.opconfig.remote);
 var config = {
     controllerConfig: {
         apiUrl: server.href.replace(server.path, ""),
         creds: outpost.config.controllerConfig.creds
     }
 }
-fs.writeFileSync("connector/config.json", JSON.stringify(config, null, 2));
+fs.writeFileSync("connector/config.json", JSON.stringify(config, null, 2));*/
 outpost.done();
