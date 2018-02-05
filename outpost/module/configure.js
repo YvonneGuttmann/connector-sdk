4d3dbf0603b428d8315b6b3fbd5041dff8bce548
Nadav Fischer <nadav.fischer@capriza.com>
2018-02-05 16:29:58 +0200

    add apiUrl to connector config after install

--- outpost/module/configure.js
+++ outpost/module/configure.js
@@ -1,14 +1,17 @@
 const fs = require("fs");
+const url = require("url");
 
 let vanilla = require("./connector/resources/_vanilla.json");
 let transformer = (outpost.config.transformer || "").trim();
 let fingerprint = (outpost.config.fingerprint || "").trim();
 let blConfig = (outpost.config.blConfig || "").trim();
-
+let parsed = url.parse(outpost.opconfig.remote);
+let apiUrl = url.format({protocol: parsed.protocol, hostname: parsed.hostname, port: parsed.port, pathname: "/v1"})
 let [apiKey, apiSecret] = outpost.opconfig.auth.split(":");
 let config = {
   caprizaConfig: {
     connectorId: outpost.config.connectorId,
+    apiUrl: apiUrl,
     creds: {
       apiKey: apiKey,
       apiSecret: apiSecret
