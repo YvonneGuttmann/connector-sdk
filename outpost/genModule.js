bcb5cef9f5f7d81ef1b94ce68ca64b7a6091ff2f
Hedva Slassi <hedva.slassi@capriza.com>
2018-02-18 12:07:20 +0200

    use controller config for deployment (instead of parts of it)

--- outpost/genModule.js
+++ outpost/genModule.js
@@ -5,15 +5,13 @@ let packageJson = JSON.parse(fs.readFileSync(process.argv[2], "utf8"));
 let configJson = JSON.parse(fs.readFileSync(process.argv[3], "utf8"));
 let moduleJsonTpl = fs.readFileSync(`module.json`, "utf8");
 
-let transformer = configJson.controllerConfig.schemaTransformer;
-let fingerprint = configJson.controllerConfig.actionFingerprint;
+let controllerConfig = configJson.controllerConfig;
 let blConfig = configJson.blConfig;
 
 let view = {
   name: packageJson.name,
   version: packageJson.version,
-  transformer: transformer ? JSON.stringify(JSON.stringify(transformer, null, 2)) : undefined,
-  fingerprint: fingerprint ? JSON.stringify(JSON.stringify(fingerprint, null, 2)) : undefined,
+  controllerConfig: controllerConfig ? JSON.stringify(JSON.stringify(controllerConfig, null, 2)) : undefined,
   blConfig: blConfig ? JSON.stringify(JSON.stringify(blConfig, null, 2)) : undefined
 }
 
--- outpost/module/configure.js
+++ outpost/module/configure.js
@@ -2,12 +2,13 @@ const fs = require("fs");
 const url = require("url");
 
 let vanilla = require("./connector/resources/_vanilla.json");
-let transformer = (outpost.config.transformer || "").trim();
-let fingerprint = (outpost.config.fingerprint || "").trim();
 let blConfig = (outpost.config.blConfig || "").trim();
 let parsed = url.parse(outpost.opconfig.remote);
 let apiUrl = url.format({protocol: parsed.protocol, hostname: parsed.hostname, port: parsed.port, pathname: "/v1"})
 let [apiKey, apiSecret] = outpost.opconfig.auth.split(":");
+
+let controllerConfig = (outpost.config.controllerConfig || "").trim();
+
 let config = {
   caprizaConfig: {
     connectorId: outpost.config.connectorId,
@@ -18,10 +19,7 @@ let config = {
     }
   },
   systemConfig: outpost.config.systemConfig,
-  controllerConfig: {
-    schemaTransformer: transformer ? JSON.parse(transformer) : vanilla.controllerConfig.schemaTransformer,
-    actionFingerprint: fingerprint ? JSON.parse(fingerprint) : vanilla.controllerConfig.actionFingerprint
-  }, 
+  controllerConfig: controllerConfig ? JSON.parse(controllerConfig) : (vanilla.controllerConfig || {}),
   blConfig: blConfig ? JSON.parse(blConfig) : vanilla.blConfig
 }
 fs.writeFileSync("connector/resources/config.json", JSON.stringify(config, null, 2));
