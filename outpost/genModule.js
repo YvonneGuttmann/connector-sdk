a88bd17915f7cc7c48df5eba77581a6d283ef0e7
Nadav Fischer <nadav.fischer@capriza.com>
2018-02-20 17:11:32 +0200

    add author capriza and version parts for sorting to module.json

--- outpost/genModule.js
+++ outpost/genModule.js
@@ -8,9 +8,17 @@ let moduleJsonTpl = fs.readFileSync(`module.json`, "utf8");
 let controllerConfig = configJson.controllerConfig;
 let blConfig = configJson.blConfig;
 
+packageJson.version = packageJson.version || "1.0.0";
+let [verMajor, verMinor, patch] = packageJson.version.split(".");
+let [verPatch, verTag] = (patch || "").split("-");
 let view = {
   name: packageJson.name,
   version: packageJson.version,
+  verMajor: parseInt(verMajor || "1"),
+  verMinor: parseInt(verMinor || "0"),
+  verPatch: parseInt(verPatch || "0"),
+  verTag: verTag || "",
+  author: packageJson.author || "capriza",
   controllerConfig: controllerConfig ? JSON.stringify(JSON.stringify(controllerConfig, null, 2)) : undefined,
   blConfig: blConfig ? JSON.stringify(JSON.stringify(blConfig, null, 2)) : undefined
 }
