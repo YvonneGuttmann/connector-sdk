ae76cbf8fecf093e2738254e0651bba3b79da024
Dror <dror.schwartz@capriza.com>
2018-03-15 14:08:42 +0200

    Refactor sync: - Support chunks - Support partialSync - Support deleted approvals - Better error handling

--- test/syncher.js
+++ test/syncher.js
@@ -6,12 +6,12 @@ var loggerFactory = new Logger("console");
 var logger = loggerFactory.create({component: "test/syncher.js"});
 
 describe ("syncher", function (){
-    it ("~1 should return hashList as removed if no data fetched", function (){
+    it("~1 should return hashList as removed if no data fetched", function (){
         var result = syncher.sync ([{id: "1",syncver: "1", private: {id: "1"}, hash: "XXX"}], [], {logger});
         result.should.deep.include ({id: "1", syncver: "1", deleted: true});
     });
 
-    it ("~2 check sync for added, removed, updated", function (){
+    it("~2 check sync for added, removed, updated", function (){
 
         var mockApprovals = [
             {private: {id: "1"}, public: {attr1: "attr1", attr2: "attr2", attr3: [{id: "line1", line_attr1: "line_attr1"}, {id: "line2", line_attr1: "line_attr2"}]}},
@@ -35,5 +35,108 @@ describe ("syncher", function (){
 
         var result = syncher.sync (mockHashList, mockApprovals, {logger});
         result.should.have.deep.members (expected.added.concat(expected.removed).concat(expected.updated));
+    });
+
+    it("~3 added", function (){
+		var mockHashList = [
+			{ id: 1, private: { id: 1, approver : "approver" }, metadata: { fingerprints: { sync: "111", action: "aaa" } } },
+        ];
+		
+        var mockApprovals = [
+			{ private: { id: 1, approver : "approver" }, metadata: { fingerprints: { sync: "111", action: "aaa" } } },
+			{ private: { id: 2, approver : "approver" }, metadata: { fingerprints: { sync: "222", action: "bbb" } } }
+        ];
+
+        var expected = [
+			{ private: { id: 2, approver : "approver" }, metadata: { fingerprints: { sync: "222", action: "bbb" } } }
+		];
+
+        syncher.sync(mockHashList, mockApprovals).should.have.deep.members(expected);
+    });
+	
+    it("~4 updated", function (){
+		var mockHashList = [
+			{ id: 1, private: { id: 1, approver : "approver" }, metadata: { fingerprints: { sync: "111", action: "aaa" } } },
+        ];
+		
+        var mockApprovals = [
+			{ private: { id: 1, approver : "approver" }, metadata: { fingerprints: { sync: "1111", action: "aaa" } } },
+        ];
+
+        var expected = [
+			{ id: 1, private: { id: 1, approver : "approver" }, metadata: { fingerprints: { sync: "1111", action: "aaa" } } },
+		];
+
+        syncher.sync(mockHashList, mockApprovals).should.have.deep.members(expected);
+    });
+		
+    it("~5 removed", function (){
+		var mockHashList = [
+			{ id: 1, private: { id: 1, approver : "approver" }, metadata: { fingerprints: { sync: "111", action: "aaa" } } },
+			{ id: 2, private: { id: 2, approver : "approver" }, metadata: { fingerprints: { sync: "aaa", action: "bbb" } } },
+        ];
+		
+        var mockApprovals = [
+			{ private: { id: 1, approver : "approver" }, metadata: { fingerprints: { sync: "111", action: "aaa" } } },
+        ];
+
+        var expected = [
+			{ id: 2, syncver: undefined, deleted: true }
+		];
+
+        syncher.sync(mockHashList, mockApprovals).should.have.deep.members(expected);
+    });
+	
+    it("~6 approval.error", function (){
+		var mockHashList = [
+			{ id: 1, private: { id: 1, approver : "approver" }, metadata: { fingerprints: { sync: "111", action: "aaa" } } },
+			{ id: 2, private: { id: 2, approver : "approver" }, metadata: { fingerprints: { sync: "222", action: "bbb" } } }
+        ];
+		
+        var mockApprovals = [
+			{ private: { id: 1 }, metadata : { fingerprints : {} }, error: "some error" },
+			{ private: { id: 2, approver : "approver" }, metadata: { fingerprints: { sync: "222", action: "bbb" } } }
+        ];
+
+        var expected = [];
+
+        syncher.sync(mockHashList, mockApprovals).should.have.deep.members(expected);
+    });
+	
+    it("~7 approval.deleted", function (){
+		var mockHashList = [
+			{ id: 1, private: { id: 1, approver : "approver" }, metadata: { fingerprints: { sync: "111", action: "aaa" } } },
+			{ id: 2, private: { id: 2, approver : "approver" }, metadata: { fingerprints: { sync: "222", action: "bbb" } } }
+        ];
+		
+        var mockApprovals = [
+			{ private: { id: 1 }, metadata : { fingerprints : {} }, deleted: true },
+			{ private: { id: 2, approver : "approver" }, metadata: { fingerprints: { sync: "222", action: "bbb" } } }
+        ];
+
+        var expected = [
+			{ id: 1, syncver: undefined, deleted: true }
+		];
+
+        syncher.sync(mockHashList, mockApprovals).should.have.deep.members(expected);
+    });
+	
+    it("~8 approver change", function (){
+		var mockHashList = [
+			{ id: 1, private: { id: 1, approver : "approver" }, metadata: { fingerprints: { sync: "111", action: "aaa" } } },
+			{ id: 2, private: { id: 2, approver : "approver" }, metadata: { fingerprints: { sync: "222", action: "bbb" } } }
+        ];
+		
+        var mockApprovals = [
+			{ private: { id: 1, approver : "approver2" }, metadata: { fingerprints: { sync: "1111", action: "aaaa" } } },
+			{ private: { id: 2, approver : "approver" }, metadata: { fingerprints: { sync: "222", action: "bbb" } } }
+        ];
+
+        var expected = [
+			{ id: 1, syncver: undefined, deleted: true },
+			{ private: { id: 1, approver : "approver2" }, metadata: { fingerprints: { sync: "1111", action: "aaaa" } } }
+		];
+
+        syncher.sync(mockHashList, mockApprovals).should.have.deep.members(expected);
     });
 });
