407c68bea3f3e0acf43564257dee7cd2be83b597
livnoni <yehuda.livnoni@capriza.com>
2018-08-16 17:54:05 +0300

    added changes for taskId

--- test/SDKTasks/utils/bl/resources/transformer.js
+++ test/SDKTasks/utils/bl/resources/transformer.js
@@ -1,61 +1,10 @@
 module.exports = {
-    schemaId: "{org}/approval/{system}/expense/generic/1.0",
+    schemaId: "{{schemaId}}",
     public: {
-        name: "{{name:string}}",
-        requester: "{{requester:string}}",
-        requestDate: "{{requestDate:date}}",
-        location: "{{location:string}}",
-        costCenter: "{{costCenter:string}}",
-        items: {
-            $fetch: "{{receipts:array}}",
-            $map: {
-                name: "{{this.name}}",
-                amount: {
-                    value: "{{this.amount.value:number}}",
-                    currency: "{{this.amount.currency:string}}"
-                },
-                date: "{{this.date:date}}",
-                category: "{{this.category:string}}"
-            }
-        },
-        history: {
-            $fetch: "{{history:array}}",
-            $map: {
-                from: "{{this.user}}",
-                status: "{{this.status}}",
-                date: "{{this.date:?date}}",
-                isOpen: {
-                    $fetch: "{{this.status}}",
-                    $translate: [
-                        {
-                            from: "Pending",
-                            to: true
-                        },
-                        {
-                            default: false
-                        }
-                    ]
-                },
-                isSelf: {
-                    $translate: [
-                        {
-                            from: {
-                                "this.userId": {
-                                    $eq: "{{approverId}}"
-                                }
-                            },
-                            to: true
-                        },
-                        {
-                            default: false
-                        }
-                    ]
-                }
-            }
-        }
+        name: "{{title:string}}",
     },
     private: {
         id: "{{id}}",
-        approver: "{{approverId}}"
+        approver: "{{approve-by}}"
     }
 };
